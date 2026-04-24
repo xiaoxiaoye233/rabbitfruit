c154bd00b66d2e897e1e72b546e1d15d82ffcc4f
xiaoxiaoye233 <3261768759@qq.com>
2026-04-24 12:50:34 +0800

    feat: 添加测试API接口和HTTP工具类
    新增testAPI.js用于测试接口调用，创建http.js作为HTTP请求工具类
    修改main.js添加测试接口调用代码
    更新package.json添加axios和sass依赖
    添加element主题样式文件并配置vite支持sass

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//测试接口函数
+import { getCategory } from '@/apis/testAPI'
+// 调用测试接口函数
+getCategory().then(res => {
+    console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
     }),
   ],
   resolve: {
@@ -27,4 +27,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
