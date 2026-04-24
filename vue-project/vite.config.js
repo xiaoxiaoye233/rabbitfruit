983f45f00cbf09e7762248aa1b8a7ba606f8b184
xiaoxiaoye233 <3261768759@qq.com>
2026-04-24 12:05:37 +0800

    feat: 添加 Element Plus 组件库及按需导入配置
    添加 Element Plus UI 组件库及其相关依赖
    配置 vite 按需导入插件
    简化 App.vue 并添加 Element 按钮组件

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,11 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+//按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    //按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
