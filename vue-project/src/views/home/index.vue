6aaf3259a672a7ac5d4c75786cf10428bed17e5e
xiaoxiaoye233 <3261768759@qq.com>
2026-04-25 11:58:01 +0800

    feat(home): 新增HomePanel组件并调整组件命名
    添加可复用的HomePanel组件用于展示标题和内容区域
    将homeProduct组件引用名改为大驼峰命名规范

--- vue-project/src/views/home/index.vue
+++ vue-project/src/views/home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,11 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <div>我是新鲜好物</div>
+  </HomePanel>
+  <HomePanel title="热门推荐" subTitle="热门推荐 品质靠谱">
+    <div>我是热门推荐</div>
+  </HomePanel>
 </template>
