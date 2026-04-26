599b5afd4a9789cddc8821429b9ef7234f3c7587
xiaoxiaoye233 <3261768759@qq.com>
2026-04-26 23:15:31 +0800

    feat(分类): 添加二级分类页面及路由功能
    - 新增二级分类API接口
    - 添加二级分类路由配置
    - 创建二级分类页面组件
    - 修改一级分类跳转链接

--- vue-project/src/apis/category.js
+++ vue-project/src/apis/category.js
@@ -8,3 +8,13 @@ export const getTopCategoryAPI = (id) => {
     }
   })
 }
+
+//获得二级分类
+export const getCategoryFilterAPI = (id) => {
+  return request({
+    url:'/category/sub/filter',
+    params:{
+      id
+    }
+  })
+}
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -3,11 +3,12 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-     {
+    {
       path: '/',
       component: Layout,
       children: [
@@ -18,9 +19,13 @@ const router = createRouter({
         {
           path: '/category/:id',
           component: Category,
+        },
+        {
+          path: '/category/sub/:id',
+          component: SubCategory,
         }
       ]
-    }, 
+    },
     {
       path: '/Login',
       component: Login,
